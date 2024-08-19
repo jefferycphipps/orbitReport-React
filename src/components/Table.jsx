ae089d9a82b91408528b72fe85b94230c4a9b3bf
Jeffery Phipps <jefferyphipps@att.net>
2024-08-19 11:56:50 -0500

    finished Assignment 6

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,18 +1,39 @@
+import './styling.css'
+
 const Table = ({ sat }) => {
+  function status(data){
+    const oper = data.operational;
+    if (oper){
+      return <td>Active</td>;
+    }
+    return <td>Inactive</td>;
+  }
+  //console.log(displaySats.map((sat, id))
   return (
     <table>
     <thead>
      <tr>
-       <th>Header TBD</th>
+       <th></th>
+     </tr>
+     <tr>
+       <th>Name</th>
+       <th>Type of Satellite</th>
+       <th>Launch Date</th>
+       <th>Status</th>
      </tr>
      </thead>
      <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
+      
+      {sat.map((data) => <tr key = {data.index}>
+        <td>{data.name}</td>
+        <td>{data.type}</td>
+        <td>{data.launchDate}</td>
+        <td> {data.operational?'Active' : 'Inactive'}</td>
+     </tr>)}
      </tbody>
    </table>
   );
 };
 
 export default Table;
+//<td>{data.operational}</td>
